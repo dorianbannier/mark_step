8afd33b587bb0759370614ee52acdccb5bc447a4
dorianbannier <46002544+dorianbannier@users.noreply.github.com>
2022-02-09 15:18:01 +0100

    Initialisation

---
+++ README.md
@@ -0,0 +1,36 @@
+# mark_step
+
+Library containing one function, namely mark_step(), allowing to automatically mark the foot contact/strike and the foot off in the current trial opened in the Vicon Nexus software.
+
+## Installation
+
+    pip install mark_step
+
+In order to use the function mark_step(), it is crucial to correctly installing the Vicon Nexus SDK.
+
+## Get started
+
+The use of mark_step is basic:
+
+``` python
+import mark_step as ms
+
+mark_step()
+```
+
+Each time you open a trial in Vicon Nexus, you simple had to execute the mark_step() command. That's all.
+In order to help in checking if the marking is accurate, it is possible to ask to the function to draw a plot of the heel movement and to place on it the foot contact and foot off it detected:
+
+``` python
+import mark_step as ms
+
+mark_step(plot = 1)
+```
+If you want to use other markers to automatically mark the event's steps:
+
+``` python
+import mark_step as ms
+
+mark_step(marker_left = 'LTOE', marker_right = 'RTOE')
+```
+
---
+++ mark_step.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  3 08:35:08 2022
+
+@author: dorian.bannier
+"""
+
+def mark_step(marker_left = 'LHEE', marker_right = 'RHEE', plot = 0):
+    """
+    Mark the foot strike/contact and the foot off in the current session opened in Vicon Nexus.
+
+    Parameters
+    ----------
+    marker_left : str, optional
+        DESCRIPTION. The default is LHEE.
+    
+    marker_right : str, optional
+        DESCRIPTION. The default is RHEE.
+    
+    plot : int, optional
+        DESCRIPTION. The default is 0.
+        A value fixed to 1 display a plot with the foot strike and the foot off of each side
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    
+    from viconnexusapi import ViconNexus
+    from scipy.signal import find_peaks
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from IPython import get_ipython
+    
+    vicon = ViconNexus.ViconNexus()
+    
+    vicon.ClearAllEvents()
+    
+    lhee = np.asarray(vicon.GetTrajectory(vicon.GetSubjectNames()[0], 'LHEE'))[2]
+    rhee = np.asarray(vicon.GetTrajectory(vicon.GetSubjectNames()[0], 'RHEE'))[2]
+    
+    lhee_peaks, _ = find_peaks(lhee, prominence = 20) # Foot Off Gauche
+    rhee_peaks, _ = find_peaks(rhee, prominence = 20) # Foot Off Droit
+    lhee_peaks_inverse, _ = find_peaks(lhee*-1, prominence = 20) # Foot Strike Gauche
+    rhee_peaks_inverse, _ = find_peaks(rhee*-1, prominence = 20) # Foot Strike Droit
+    
+    [vicon.CreateAnEvent(vicon.GetSubjectNames()[0], 'Left', 'Foot Off', int(i), 0) for i in lhee_peaks]
+    [vicon.CreateAnEvent(vicon.GetSubjectNames()[0], 'Right', 'Foot Off', int(i), 0) for i in rhee_peaks]
+    [vicon.CreateAnEvent(vicon.GetSubjectNames()[0], 'Left', 'Foot Strike', int(i), 0) for i in lhee_peaks_inverse]
+    [vicon.CreateAnEvent(vicon.GetSubjectNames()[0], 'Right', 'Foot Strike', int(i), 0) for i in rhee_peaks_inverse]
+    
+    if plot == 1:
+        get_ipython().run_line_magic('matplotlib', 'qt')
+        fig, axes = plt.subplots(2, 2, figsize=(8, 8), sharex=True)
+        axes[0,0].plot(lhee)
+        axes[0,0].plot(lhee_peaks, lhee[lhee_peaks], "x")
+        axes[0,0].title.set_text('Foot Off - Gauche')
+        
+        axes[1,0].plot(lhee)
+        axes[1,0].plot(lhee_peaks_inverse, lhee[lhee_peaks_inverse], "x")
+        axes[1,0].title.set_text('Foot Strike - Gauche')
+        
+        axes[0,1].plot(rhee)
+        axes[0,1].plot(rhee_peaks, rhee[rhee_peaks], "x")
+        axes[0,1].title.set_text('Foot Off - Droit')
+        
+        axes[1,1].plot(rhee)
+        axes[1,1].plot(rhee_peaks_inverse, rhee[rhee_peaks_inverse], "x")
+        axes[1,1].title.set_text('Foot Strike - Droit')
+        fig.show()
